2a8a595e7c815a3f4cbfe79543fc165f9c7d170f
Emin <pandnyr@hotmail.com>
2023-01-01 11:45:50 +0300

    first commit

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -5,18 +5,66 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchCandyMachine: FC = () => {
-  const [candyMachineAddress, setCandyMachineAddress] = useState(null)
+  const [candyMachineAddress, setCandyMachineAddress] = useState("CM_ADDRESS_HERE")
   const [candyMachineData, setCandyMachineData] = useState(null)
   const [pageItems, setPageItems] = useState(null)
   const [page, setPage] = useState(1)
+  
+  const { connection } = useConnection()
+  const metaplex = Metaplex.make(connection)
 
-  const fetchCandyMachine = async () => {}
+  // fetch candymachine by address
+  const fetchCandyMachine = async () => {
+    
+    // Set page to 1 - we wanna be at the first page whenever we fetch a new Candy Machine
+    setPage(1)
 
-  const getPage = async (page, perPage) => {}
+    // fetch candymachine data
+    try {
+      const candyMachine = await metaplex
+        .candyMachines()
+        .findByAddress({ address: new PublicKey(candyMachineAddress) })
+        .run()
 
-  const prev = async () => {}
+      setCandyMachineData(candyMachine)
+    } catch (e) {
+      alert("Please submit a valid CMv2 address.")
+    }
+  }
 
-  const next = async () => {}
+  // paging
+  const getPage = async (page, perPage) => {
+    const pageItems = candyMachineData.items.slice(
+      (page - 1) * perPage,
+      page * perPage
+    )
+
+    // fetch metadata of NFTs for page
+    let nftData = []
+    for (let i = 0; i < pageItems.length; i++) {
+      let fetchResult = await fetch(pageItems[i].uri)
+      let json = await fetchResult.json()
+      nftData.push(json)
+    }
+
+    // set state
+    setPageItems(nftData)
+  }
+
+   // previous page
+   const prev = async () => {
+    if (page - 1 < 1) {
+      setPage(1)
+    } else {
+      setPage(page - 1)
+    }
+  }
+
+  // next page
+  const next = async () => {
+    setPage(page + 1)
+  }
+
 
   return (
     <div>
--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -4,7 +4,69 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchNft: FC = () => {
-  const fetchNfts = async () => {}
+  const [nftData, setNftData] = useState(null)
 
-  return <div></div>
+  const { connection } = useConnection()
+  const wallet = useWallet()
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet))
+
+    const fetchNfts = async () => {
+    if (!wallet.connected) {
+      return
+    }
+
+    // fetch NFTs for connected wallet
+    const nfts = await metaplex
+      .nfts()
+      .findAllByOwner({ owner: wallet.publicKey })
+
+    // fetch off chain metadata for each NFT
+    let nftData = []
+    for (let i = 0; i < nfts[1].lenght; i++) {
+      let fetchResult = await fetch(nfts[i].uri)
+      let json = await fetchResult.json()
+      nftData.push(json)
+    }
+
+    // set state
+    setNftData(nftData)
+  }
+
+  // fetch nfts when connected wallet changes
+  useEffect(() => {
+    fetchNfts()
+  }, [wallet])
+
+  // // fetch placeholder candy machine on load
+  // useEffect(() => {
+  //   fetchCandyMachine()
+  // }, [])
+
+  // // fetch metadata for NFTs when page or candy machine changes
+  // useEffect(() => {
+  //   if (!candyMachineData) {
+  //     return
+  //   }
+  //   getPage(page, 9)
+  // }, [candyMachineData, page])
+
+
+  return ( <div>
+    {nftData && (
+      <div className={styles.gridNFT}>
+        {nftData.map((nft) => (
+          <div>
+            <ul>{nft.name}</ul>
+            <img src={nft.image} />
+          </div>
+        ))}
+      </div>
+    )}
+  </div>)
 }
+
+
+function fetchCandyMachine() {
+  throw new Error("Function not implemented.")
+}
+
